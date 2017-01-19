0d8b933c261f20e7b77d576c2a660894ce7375cc
Kelsey <kpnewman2013@gmail.com>
2017-01-19 15:34:08 -0500

    added necessary files

---
+++ Purchases/src/main/java/com/theironyard/PurchasesController.java
@@ -0,0 +1,10 @@
+package com.theironyard;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by kelseynewman on 1/19/17.
+ */
+@Controller
+public class PurchasesController {
+}
